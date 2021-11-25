318da3ab65d151d283302dc4c480ce0c952d35e2
hodhod <alameddinehodhod@gamail.com>
2021-11-25 16:46:03 +0200

    step 4

---
+++ javascript-basics-01/step-4.js
@@ -0,0 +1,6 @@
+var name = prompt("What is your name ?");
+var surname = prompt("What is your Surname ?");
+var city = prompt("Which city are you from ?");
+
+alert( "Name : " + name + "\n" + "Surname : " + surname + "\n" + "City : " + city);
+
