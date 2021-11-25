fdd481b67bc5b0ce4fd71aeccf09d74568c7aadf
hodhod <alameddinehodhod@gamail.com>
2021-11-25 17:20:08 +0200

    step 5

---
+++ javascript-basics-01/step-5.js
@@ -0,0 +1,8 @@
+
+function calculate() {
+    var fNb = document.getElementById('first_number').value;
+    var sNb = document.getElementById('second_number').value;
+    var result = fNb * sNb;
+
+     alert(" result of both numbers is: " + result);
+}   
